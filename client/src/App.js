644a1d3cc429a3ef40801472c587fcc56c7d9c44
herbiemolina <herbiemolina@gmail.com>
2019-05-15 11:15:06 -0400

    sign up page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
+import SignUp from "./pages/SignUp";
 
 function App() {
   return (
     <Router>
       <div>
         <Route exact path="/" component={LandingPage} />
+        <Route exact path="/signup" component={SignUp} />
+
       </div>
     </Router>
   );
