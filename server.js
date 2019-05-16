7520953d5d7c01890384bccd0f0301cb66a7ffbf
SydPRetzel <myskandev@gmail.com>
2019-05-16 12:58:01 -0400

    Reorganize folders such that passport based login code works with single server.

--- server.js
+++ server.js
@@ -1,20 +1,33 @@
 const express = require("express");
+const bodyParser = require("body-parser")
+const morgan = require("morgan")
+const session = require("express-session")
 
 const mongoose = require("mongoose");
 const routes = require("./routes");
+// Route requires
+const user = require('./routes/user');
+const passport = require("./passport");
 const app = express();
 const PORT = process.env.PORT || 3001;
-const session = require('express-session');
-const passport = require('passport');
-
 
 // Define middleware here
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+//app.use(express.urlencoded({ extended: true }));
+//app.use(express.json());
+// TODO : What is the difference between this parser and the
+// express parser and json?
+app.use(morgan('dev'))
+app.use(
+	bodyParser.urlencoded({
+		extended: false
+	})
+)
+app.use(bodyParser.json())
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-};
+}
 
 // Sessions
 app.use(
@@ -26,12 +39,11 @@ app.use(
 );
 
 // Passport
-app.use(passport.initialize())
-app.use(passport.session()) // calls the deserializeUser
-
-
+app.use(passport.initialize());
+app.use(passport.session()) // calls the deserializeUser;
 
 // Add routes, both API and view
+// DO THIS AFTER INITIALIZING PASSPORT!!
 app.use(routes);
 
 // Connect to the Mongo DB
@@ -41,6 +53,3 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/underdogdb");
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
-
-
-module.exports = mongoose.connection
