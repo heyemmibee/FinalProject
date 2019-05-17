6b9a5574040deb317bdffa979f5e663d72478f5a
SydPRetzel <myskandev@gmail.com>
2019-05-17 13:13:32 -0400

    Integrated to dashboard.

--- client/src/pages/Dashboard/index.js
+++ client/src/pages/Dashboard/index.js
@@ -1,29 +1,53 @@
 import React from "react";
-import "./dashboard.css";
 import { Container, Row, Col, Nav } from "react-bootstrap";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
+import ChatApp from "../../components/ChatApp";
+
+import "./dashboard.css";
+
 
 function Dasboard() {
-    return (
-        <div>
-            <Nav className="navbar">
-                <h1 id="navTitle" ><img src="./images/hands2.png" width="45" height="45" class="d-inline-block align-top" alt="" />  HelpHub</h1>
-                <div className="ml-auto">
-                    <Link to="/profile"class="link"><h4 className="links d-inline">Profile  </h4></Link>
-                    <Link to="/login" className="link"><h4 className="links d-inline">|  Logout</h4></Link>
-                </div>
-            </Nav>
-            <Container>
+  // TODO : Grab username from express session here.
+  let username = "RandomUser";
+  console.log("username : " + username);
 
-                <Row>
-                    <Col>
-                    </Col>
-                </Row>
-            </Container>
+  return (
+    <div>
+      <Nav className="navbar">
+        <h1 id="navTitle">
+          <img
+            src="./images/hands2.png"
+            width="45"
+            height="45"
+            className="d-inline-block align-top"
+            alt=""
+          />{" "}
+          HelpHub
+        </h1>
+        <div className="ml-auto">
+          <Link to="/profile" className="link">
+            <h4 className="links d-inline">Profile </h4>
+          </Link>
+          <Link to="/login" className="link">
+            <h4 className="links d-inline">| Logout</h4>
+          </Link>
         </div>
-    )
+      </Nav>
+      <Container>
+        <Row>
+          <Col />
+        </Row>
+        <Row>
+          <Col>
+            <ChatApp username={username} />
+          </Col>
+        </Row>
+        <Row>
+          <Col />
+        </Row>
+      </Container>
+    </div>
+  );
 }
 
-
-
-export default Dasboard
+export default Dasboard;
