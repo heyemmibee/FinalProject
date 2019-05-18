65f30ce12a0800cf8d623a27cdc2de11f6c3aa2d
SydPRetzel <myskandev@gmail.com>
2019-05-17 23:52:47 -0400

    Added default user icon.

--- client/src/components/OnlineUser/index.js
+++ client/src/components/OnlineUser/index.js
@@ -2,7 +2,18 @@ import React from "react";
 
 class OnlineUser extends React.Component {
   render() {
-    return <div className="online_user">{this.props.username}</div>;
+    return (
+      <div className="online_user">
+        <img
+          src="./images/female_user.png"
+          width="45"
+          height="45"
+          className="d-inline-block align-top"
+          alt="female_user"
+        />
+        {this.props.username}
+      </div>
+    );
   }
 }
 
