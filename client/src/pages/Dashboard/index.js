cee752b7edf1223423b1c4ef70aa8e4f12aeab2a
SydPRetzel <myskandev@gmail.com>
2019-05-20 14:29:18 -0400

    Signup page mods.

--- client/src/pages/Dashboard/index.js
+++ client/src/pages/Dashboard/index.js
@@ -46,6 +46,7 @@ class Dasboard extends Component {
               HelpHub
             </h1>
             <div className="ml-auto">
+            <h4>Hello {this.username}!</h4>
               <Link to="/profile" className="link">
                 <h4 className="links d-inline">Profile </h4>
               </Link>
