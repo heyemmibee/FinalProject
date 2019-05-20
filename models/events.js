927fd65ff4e445a7f9766044e325098ba50f2ca9
eburnaman <eburnaman@gmail.com>
2019-05-20 14:37:27 -0400

    events model

---
+++ models/events.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+mongoose.promise = Promise
+
+// Ref Mongoose Schema Constructor//
+const Schema = mongoose.Schema;
+const eventsSchema = new Schema({
+
+    //userid from login? Help
+    creator: { 
+        type: Schema.ObjectId, 
+        ref: 'User'
+    },
+    title: { 
+        type: String, 
+        index:true 
+    },
+    location: { 
+        type: String, 
+        index:true 
+    },
+    description: {
+        type: String,
+        maxlength: 300
+    },
+    category: [{ 
+        type: Schema.ObjectId, 
+        ref: 'category'
+    }],
+    date: { 
+        type: Date, 
+        default: Date.now,
+        timestamp: true
+    },
+  });
+
+const events = mongoose.model('events', eventsSchema)
+module.exports = events
