728c4a31e95fd915db7411e6f02e7ab6b209fe48
SydPRetzel <myskandev@gmail.com>
2019-05-22 15:53:54 -0400

    Heroku seeding.

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -1,51 +1,9 @@
 const mongoose = require("mongoose");
-const profile = require("../models/profile");
 const events = require("../models/events");
 
 // This file empties the collection and inserts predefined values.
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/underdogdb");
-
-// const profileSeed = [
-//   {
-//     name: "Sir Helps-A-Lot",
-//     email: "helpsalot@yahoo.com",
-//     phone: "123-456-7890",
-//     city: "Arlington",
-//     state: "VA",
-//     zip: "22201",
-//     date: new Date(Date.now())
-//   },
-//   {
-//     name: "Madams Organ",
-//     email: "admo@gmail.com",
-//     phone: "321-654-0987",
-//     city: "Washington",
-//     state: "DC",
-//     zip: "20001",
-//     date: new Date(Date.now())
-//   },
-//   {
-//     name: "Honor Grace",
-//     email: "angel@aol.com",
-//     phone: "000-000-0000",
-//     city: "Silver Spring",
-//     state: "MD",
-//     zip: "70345",
-//     date: new Date(Date.now())
-//   }
-// ];
-
-// profile
-//   .remove({})
-//   .then(() => profile.collection.insertMany(profileSeed))
-//   .then(data => {
-//     console.log(data.result.n + " profiles inserted!");
-//   })
-//   .catch(err => {
-//     console.error(err);
-//     process.exit(1);
-//   });
 
 //Event Seed categories for reference: Cooking & Baking, Community Cleanups, Teaching & Training, Gardening & Landscaping
 // Events & Advertising, Environment & Wildlife, Mental Health & Fitness, Harvest & Farming, Home Improvement & DIY
